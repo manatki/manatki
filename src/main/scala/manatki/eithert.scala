e4e6f648a9e67130b987c1cc87bbdeef1d3f3be8
Oleg Nizhnik <o.nizhnikov@tinkoff.ru>
2018-02-19 14:02:48 +0300

    lifterror

---
+++ src/main/scala/manatki/eithert.scala
@@ -0,0 +1,12 @@
+package manatki
+
+import cats.ApplicativeError
+import cats.data.EitherT
+
+object eithert {
+  implicit class EitherTOps[F[_], E, A](val x: EitherT[F, E, A]) extends AnyVal {
+    /** lifting errors from underlying functor */
+    def liftError[U](f: U => E)(implicit err: ApplicativeError[F, U]): EitherT[F, E, A] =
+      EitherT(err.handleError(x.value)(e => Left(f(e))))
+  }
+}
