1ffb740f9ecb1bcd404e41fafa36c1139a1770ce
Odomontois <odomontois@gmail.com>
2019-07-23 11:03:23 +0300

    solved

--- problems/src/test/scala/manatki/problems/CyclicTrainSuite.scala
+++ problems/src/test/scala/manatki/problems/CyclicTrainSuite.scala
@@ -1,11 +1,8 @@
 package manatki.problems
 
-import cats.data.State
-import manatki.problems.CyclicTrain.{CT, countWagons, cyclicTrainState}
-import org.scalatest.{FlatSpec, Matchers}
+import manatki.problems.CyclicTrain.{countWagons, cyclicTrainState}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
-
-import scala.collection.immutable.BitSet
+import org.scalatest.{FlatSpec, Matchers}
 
 class CyclicTrainSuite extends FlatSpec with GeneratorDrivenPropertyChecks with Matchers {
   "Solution" should "work for any input" in {
