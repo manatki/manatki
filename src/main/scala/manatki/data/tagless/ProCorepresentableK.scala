d3d0e4c7efcf17994334b38d50998ced24b686da
Oleg Nizhnik <o.nizhnikov@tinkoff.ru>
2019-12-26 10:58:44 +0300

    Construction of higher kinded recursive datatypes with ProCorecursive algebras

--- src/main/scala/manatki/data/tagless/ProCorepresentableK.scala
+++ src/main/scala/manatki/data/tagless/ProCorepresentableK.scala
@@ -1,11 +1,12 @@
 package manatki.data.tagless
 import cats.arrow.FunctionK
 import cats.tagless.FunctorK
-import cats.{Functor, ~>}
+import cats.~>
+import manatki.data.tagless.ProCorepresentable.tabulate
 import manatki.syntax.funK
 import tofu.higherKind.RepK
 
-trait ProfunctorK[P[i[_], o[_]]] {
+trait ProfunctorK[P[_[_], _[_]]] {
   def leftMapK[I[_], J[_], O[_]](pio: P[I, O])(fk: J ~> I): P[J, O]
   def rightMapK[I[_], O[_], Q[_]](pio: P[I, O])(fk: O ~> Q): P[I, Q]
 
@@ -13,7 +14,7 @@ trait ProfunctorK[P[i[_], o[_]]] {
     leftMapK(rightMapK(pio)(foq))(fji)
 }
 
-trait ProCorepresentableK[P[i[_], o[_]]] extends ProfunctorK[P] {
+trait ProCorepresentableK[P[_[_], _[_]]] extends ProfunctorK[P] {
   def tabulate[I[_], O[_]](hom: RepK[P[I, *[_]], *] ~> O): P[I, O]
 
   override def rightMapK[I[_], O[_], Q[_]](fab: P[I, O])(f: O ~> Q): P[I, Q] =
@@ -24,4 +25,12 @@ trait ProCorepresentableK[P[i[_], o[_]]] extends ProfunctorK[P] {
       def mapK[F[_], G[_]](af: RepK[P[F, *[_]], A])(fk : F ~> G): RepK[P[G, *[_]], A] =
         af(leftMapK(tabulate(FunctionK.id[RepK[P[G, *[_]], *]]))(fk))
     }
+}
+
+object ProCorepresentableK{
+  def tabulate[P[_[_], _[_]], F[_], G[_]](k: RepK[P[F, *[_]], *] ~> G)(implicit P: ProCorepresentableK[P]): P[F, G] =
+    P.tabulate(k)
+
+  def construct[P[-_[_], +_[_]]: ProCorepresentableK]: P[Layer1[P, *], Layer1[P, *]] =
+    tabulate[P, Layer1[P, *], Layer1[P, *]](funK(_(construct)))
 }
