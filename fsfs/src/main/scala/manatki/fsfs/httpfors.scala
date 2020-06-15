864587037e95956513a5a617f7c2609915063246
Odomontois <odomontois@gmail.com>
2020-06-15 11:44:05 +0300

    comment

--- fsfs/src/main/scala/manatki/fsfs/httpfors.scala
+++ fsfs/src/main/scala/manatki/fsfs/httpfors.scala
@@ -14,6 +14,7 @@ object httpfors {
   def imapRoutes[F[_], G[_]: Functor](routes: HttpRoutes[F])(implicit FG: IsoK[F, G]): HttpRoutes[G] = 
     Kleisli(greq => routes.run(greq.mapK(FG.fromF)).mapK(FG.tof).map(_.mapK(FG.tof)))
 
+  // requested bu LolDog in PONV 15.06.2020 https://t.me/scala_ponv/972282
   def unliftRoutes[F[_], G[_]: Monad](routes: HttpRoutes[F])(implicit FG: Unlift[F, G]): HttpRoutes[G] = 
     Embed.of[Http[*[_], G], OptionT[G, *]](OptionT.liftF(FG.subIso.map(implicit iso => imapRoutes[F, G](routes))))
 }
