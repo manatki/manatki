1af63ff9563fa504e25b993be6067a722d180059
Oleg Nizhnik <odomontois@gmail.com>
2021-07-16 14:58:47 +0300

    tailrecEither

---
+++ src/main/scala/manatki/to_fu/calct.scala
@@ -0,0 +1,20 @@
+package manatki.to_fu
+
+import cats.Monad
+import tofu.syntax.monadic._
+import tofu.data.CalcT
+import tofu.data.calc.StepResult._
+
+// requested in @tofu_ru by @sigevsky 16.07.2021 12:12
+object calct {
+  // run endofunctorial CalcT using monadic tailrec
+  def tailrecEither[F[+_]: Monad, R, S1, S2, E, A](
+      calc: CalcT[F, R, S1, S2, E, A]
+  )(r: R, init: S1): F[(S2, Either[E, A])] =
+    calc.step(r, init).tailRecM {
+      case Ok(state, value)                    => Right((state, Right(value))).pure[F]
+      case Error(state, err)                   => Right((state, Left(err))).pure[F]
+      case wrap: Wrap[_, r, s, S2, x, E, m, A] =>
+        wrap.inner.map(x => Left(wrap.cont.success(wrap.state, x).step(wrap.input, wrap.state)))
+    }
+}
