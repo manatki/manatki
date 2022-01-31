58b2c9d6e55b4719ca83ca9ddabd901c974eaddb
Oleg Nizhnik <odomontois@gmail.com>
2022-01-31 21:27:57 +0300

    -r

--- src/main/scala/manatki/concurrent/MemoStream.scala
+++ src/main/scala/manatki/concurrent/MemoStream.scala
@@ -8,42 +8,48 @@ import MemoData._
 import zio.Ref
 import zio.Promise
 import zio.UIO
+import zio.IO
 
-sealed trait MemoData[-R, +E, +A] {
-  def stream: ZStream[R, E, A] = this match {
+sealed trait MemoData[+E, +A] {
+  def stream: ZStream[Any, E, A] = this match {
     case End                   => ZStream.empty
     case Continue(chunk, next) => ZStream.fromChunk(chunk) ++ ZStream.fromEffect(next).flatMap(_.stream)
   }
 }
 
 object MemoData {
-  type MemoPromise[-R, +E, +A] = ZIO[R, E, MemoData[R, E, A]]
-  case object End                                                           extends MemoData[Any, Nothing, Nothing]
-  case class Continue[R, E, A](chunk: Chunk[A], next: MemoPromise[R, E, A]) extends MemoData[R, E, A]
+  type MemoPromise[+E, +A] = IO[E, MemoData[E, A]]
+  case object End                                                        extends MemoData[Nothing, Nothing]
+  case class Continue[R, E, A](chunk: Chunk[A], next: MemoPromise[E, A]) extends MemoData[E, A]
 
 }
 
-case class MemoTopic[R, E, A](
-    start: MemoPromise[R, E, A],
-    current: Ref[Promise[E, MemoData[R, E, A]]],
+case class MemoTopic[E, A](
+    start: MemoPromise[E, A],
+    current: Ref[Promise[E, MemoData[E, A]]],
     lock: Semaphore
 ) {
-  def push(data: Chunk[A]): UIO[Boolean] =
+
+  def publishIO(data: IO[E, Chunk[A]]): UIO[Boolean] =
     lock
       .withPermit((for {
-        promise <- Promise.make[E, MemoData[R, E, A]]
+        promise <- Promise.make[E, MemoData[E, A]]
         prev    <- current.modify(prev => (prev, promise))
-        result  <- prev.complete(UIO.succeed(Continue(data, promise.await)))
+        result  <- prev.complete(data.map(Continue(_, promise.await)))
       } yield result).uninterruptible)
+
+  def publish(data: Chunk[A]): UIO[Boolean] = publishIO(UIO.succeed(data))
 
   def stop: UIO[Unit] = lock.withPermit(current.get.flatMap(_.complete(UIO.succeed(End))).unit)
 
-  def stream: ZStream[R, E, A] = ZStream.fromEffect(start).flatMap(_.stream)
+  def fail(err: E): UIO[Boolean] = publishIO(IO.fail(err))
+
+  def stream: ZStream[Any, E, A] = ZStream.fromEffect(start).flatMap(_.stream)
 }
 
 object MemoTopic {
-  def create[R, E, A]: UIO[MemoTopic[R, E, A]] = for {
-    promise <- Promise.make[E, MemoData[R, E, A]]
+  def create[E, A]: UIO[MemoTopic[E, A]] = for {
+    promise <- Promise.make[E, MemoData[E, A]]
     sem     <- Semaphore.make(1)
     ref     <- Ref.make(promise)
   } yield MemoTopic(promise.await, ref, sem)
